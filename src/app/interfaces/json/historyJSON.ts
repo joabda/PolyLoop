b03b930283e15157a2c3ec6613a28388faa74562
joeA <joeA>
2020-06-20 10:40:24 -0400

    Adding features to hyperloop and changing history

--- src/app/interfaces/json/historyJSON.ts
+++ src/app/interfaces/json/historyJSON.ts
@@ -1,4 +1,5 @@
 export interface HistoryJSON {
-
+    question: string,
+    features: {title: string, detail: string, icon: string}[],
     events: {date: string, title: string, text: string, more?: string, imgSrc?: string}[],
 }
