5f5feda7ce32b3a9e828b2725d06bc691949aa8d
joeA <joeA>
2020-06-23 17:41:48 -0400

    Nav bar white all the way

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-intro',
@@ -6,32 +6,5 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./intro.component.scss']
 })
 export class IntroComponent {
-
-  private mainNav;
   constructor() { }
-
-  ngAfterViewInit(): void {
-    this.mainNav = document.getElementById('main-nav');
-    if (this.mainNav !== null) {
-      this.mainNav.style.backgroundColor = 'transparent';
-    }
-  }
-
-  ngOnDestroy(): void {
-    if (this.mainNav !== null) {
-      this.mainNav.style.backgroundColor = '#FFFFFF';
-    }
-  }
-
-  @HostListener('window:scroll', ['$event'])
-  scrollHandler(event: Event) {
-    const missionSection = document.getElementById('mission');
-    if (missionSection !== null) {
-      if (missionSection.getBoundingClientRect().top < 200) {
-        this.mainNav.style.backgroundColor = '#FFFFFF';
-      } else {
-        this.mainNav.style.backgroundColor = 'transparent';
-      }
-    }
-  }
 }
