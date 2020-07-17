38f3c27166234ae62c03c40925b4749e37e31d21
joeA <joeA>
2020-07-17 18:57:34 -0400

    Mobile and Table updates for Contact Us

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -1,20 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { DataService } from 'src/app/services/data/data.service';
 import { ContactUsJSON } from 'src/app/interfaces/json/ContactUsJSON';
 import { HttpStatus } from 'src/app/enums/http-codes';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import * as _info from 'src/assets/data/infos.json'
+import { Observable } from 'rxjs';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { shareReplay, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.scss']
 })
-export class ContactUsComponent {
-  
+export class ContactUsComponent implements OnInit {
+
   emailRegEx: RegExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-  info = (_info as any).default[0] as {phoneNumber: string, email: string};
+  info = (_info as any).default[0] as { phoneNumber: string, email: string };
   currentValues: {
     name: string,
     email: string,
@@ -24,14 +27,30 @@ export class ContactUsComponent {
   text: ContactUsJSON;
   static PROXY = 'https://cors-anywhere.herokuapp.com/';
 
-  constructor(private snacks: MatSnackBar, private http: HttpClient, private data: DataService) {
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private snacks: MatSnackBar,
+    private http: HttpClient, private data: DataService,
+    private breakpointObserver: BreakpointObserver) {
     data.language.subscribe(() => {
       this.text = data.getContactUs();
     });
-
     this.resetInputs();
-   }
+  }
 
+  ngOnInit(): void {
+    this.breakpointObserver
+      .observe(['(min-width: 585px)'])
+      .subscribe((size: BreakpointState) => {
+        const element = document.getElementById('info-cont');
+        element.classList.remove("d-flex");
+      }
+      );
+  }
 
   submit(): void {
     try {
@@ -41,17 +60,17 @@ export class ContactUsComponent {
         headers.append('Access-Control-Allow-Origin', '*');
         this.http.post(`${ContactUsComponent.PROXY}https://docs.google.com/forms/d/e/1FAIpQLSePkTgeZOEqt_K6uYSCocK9_n00Wm-e1L1-nhUUs30f9clxMA/formResponse?` +
           this.getURLData(),
-          {headers: headers}
+          { headers: headers }
         )
-        .toPromise()
-        .catch(res => {
-          if((res as HttpErrorResponse).status == HttpStatus.OK ) {
-            this.openSnack(this.data.getSent());
-            this.resetInputs();
-          } else {
-            this.openSnack(this.data.errorMessage());
-          }
-        })
+          .toPromise()
+          .catch(res => {
+            if ((res as HttpErrorResponse).status == HttpStatus.OK) {
+              this.openSnack(this.data.getSent());
+              this.resetInputs();
+            } else {
+              this.openSnack(this.data.errorMessage());
+            }
+          })
       }
     } catch (e) {
       this.openSnack(this.data.errorMessage());
