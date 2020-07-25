2e98bafc5ad99023220d03924d4564b794eedfdd
joeA <joeA>
2020-07-25 12:13:12 -0400

    Modification

--- src/app/interfaces/json/historyJSON.ts
+++ src/app/interfaces/json/historyJSON.ts
@@ -1,5 +1,7 @@
 export interface HistoryJSON {
     question: string,
     features: {title: string, detail: string, icon: string}[],
+    comp: string,
+    compElements: string[],
     events: {date: string, title: string, text: string, more?: string, imgSrc?: string}[],
 }
