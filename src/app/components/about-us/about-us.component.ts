d9ceeafa3e5997414b28e95c3449daf44c373a5a
joeA <joeA>
2020-08-01 11:54:43 -0400

    Adding team members

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { DataService } from 'src/app/services/data/data.service';
 import { AboutJSON } from 'src/app/interfaces/json/aboutJSON';
 import { FeatureItem } from 'src/app/interfaces/feature-item';
+import { TeamJSON } from 'src/app/interfaces/json/teamJSON';
 
 @Component({
   selector: 'app-about-us',
@@ -13,10 +14,14 @@ export class AboutUsComponent {
   text: AboutJSON;
   col1: FeatureItem[];
   col2: FeatureItem[];
+  teams: TeamJSON[];
+  teamTitle: string[];
 
   constructor(private data: DataService) { 
     this.data.language.subscribe( () => {
       this.text = data.getAbout();
+      this.teams = data.getMembers();
+      this.teamTitle = data.getTitles().splice(2,1);
       const center = this.text.features.length / 2;
       this.col1 = this.text.features.slice(0, center);
       this.col2 = this.text.features.slice(center);
